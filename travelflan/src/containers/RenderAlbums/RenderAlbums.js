ae09fcc25d16349e8bc2bfdf1a4e90cf7eb5ca2c
younho <nnyyhh9247@gmail.com>
2021-06-06 21:56:00 +0900

    [수정] Refactoring
    1. 페이지 렌더링 container 추가

--- travelflan/src/containers/RenderAlbums/RenderAlbums.js
+++ travelflan/src/containers/RenderAlbums/RenderAlbums.js
@@ -1,10 +1,9 @@
 import List from "components/List/List";
-import { useEffect, useState } from "react";
+import RenderPageNumber from "containers/RenderPageNumber/RenderPageNumber";
+import { useState } from "react";
 
 const RenderAlbums = ({ className, state, setState, isOpen, setIsOpen, setUpdatePost }) => {
   const [renderList, setRenderList] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  // const [isOpen, setIsOpen] = useState(false);
 
   const render = async () => {
     const data = await fetch("https://jsonplaceholder.typicode.com/albums");
@@ -26,56 +25,21 @@ const RenderAlbums = ({ className, state, setState, isOpen, setIsOpen, setUpdate
     setUpdatePost({ id: +e.target.parentNode.id, title: renderList.find(item => item.id === +e.target.parentNode.id).title });
   };
 
-  const pageNumber = () => {
-    let answer = [];
-    const number = !state.length % 5 ? state.length / 5 + 1 : state.length / 5;
-    for (let i = 0; i < number; i++) {
-      answer.push(
-        <List>
-          <button>{i + 1}</button>
-        </List>
-      );
-    }
-    return answer;
-  };
-
-  const onClick = e => {
-    if (!e.target.matches("button")) return;
-    console.log(e.target.textContent);
-    if (e.target.textContent === "<") {
-      setCurrentPage(currentPage - 1);
-    } else if (e.target.textContent === ">") {
-      setCurrentPage(currentPage + 1);
-    } else {
-      setCurrentPage(+e.target.textContent);
-    }
-  };
-
-  useEffect(() => {
-    setRenderList(() => state.slice(5 * (currentPage - 1), 5 * (currentPage - 1) + 5));
-  }, [state, currentPage]);
-
   return (
     <section className={className}>
       <ul>
         {renderList.map(album => {
           return (
-            <>
-              <List id={album.id} key={album.id}>
-                <img src="http://placehold.it/300x200" />
-                {album.title}
-                <button onClick={DeletePost}>X</button>
-                <button onClick={UpdateButton}>수정</button>
-              </List>
-            </>
+            <List id={album.id} key={album.id}>
+              <img src="http://placehold.it/300x200" />
+              {album.title}
+              <button onClick={DeletePost}>X</button>
+              <button onClick={UpdateButton}>수정</button>
+            </List>
           );
         })}
       </ul>
-      <ul className="pageNumber" onClick={onClick}>
-        <button>{"<"}</button>
-        {pageNumber()}
-        <button>{">"}</button>
-      </ul>
+      <RenderPageNumber state={state} setRenderList={setRenderList} />
     </section>
   );
 };
